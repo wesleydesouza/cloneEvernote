d5df6984de9a9e1b583fa100f73348fcad00a1ff
Wesley de Souza <wesley_duarte122@hotmail.com>
2022-06-02 11:54:13 -0300

    Token

---
+++ app/routes/users.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const secret = process.env.JWT_TOKEN;
+
+
+router.post("/register", async(req, res) => {
+  const { name, email, password } = req.body;
+  const user = new User({ name, email, password});
+
+  try {
+    await user.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({error: "Error registering new user"});
+  }
+})
+
+router.post("/login", async (req,res) => {
+  const {email, password} = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if(!user){
+      res.status(401).json({error: "Incorrect email or password"});
+    } else {
+      user.isCorrectPassword(password, function(err, same){
+        if(!same){
+          res.status(401).json({error: "Incorrect email or password"});
+        }else {
+          const token = jwt.sign({email},secret, { expiresIn: "1d"});
+          res.json({user, token});
+        }
+      })
+    }
+  } catch (error) {
+    res.status(500).json({error: "Internal error, please try again"})
+  }
+})
+
+module.exports = router;
